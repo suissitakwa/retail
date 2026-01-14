1a7dd1d880f4529c510fe1337c4a01a0fb967fc8
suissitakwa <suissitakwa@gmail.com>
2026-01-14 14:37:25 -0800

    Update CI/CD pipeline

--- src/main/java/com/retail_project/config/WebConfig.java
+++ src/main/java/com/retail_project/config/WebConfig.java
@@ -1,7 +1,14 @@
 package com.retail_project.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -9,9 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         // Allows all origins (or you can specify: "http://localhost:3000")
         registry.addMapping("/**") // Apply to all API paths
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")// Set to your frontend's origin
+                .allowedOrigins("http://localhost:3000") // Set to your frontend's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow common HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Important if you use session cookies or authentication
     }
+
 }
