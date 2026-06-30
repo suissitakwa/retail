2cf33bbdb3210c5efbf79e1d42bc37ade9c945ca
suissitakwa <suissitakwa@gmail.com>
2026-06-30 13:44:24 -0700

    make email From address configurable via APP_MAIL_FROM

--- src/main/java/com/retail_project/email/EmailService.java
+++ src/main/java/com/retail_project/email/EmailService.java
@@ -20,7 +20,7 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
-    @Value("${spring.mail.username}")
+    @Value("${app.mail.from:${spring.mail.username}}")
     private String fromAddress;
 
     @Async
