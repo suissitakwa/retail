6e82b1b8a1ce6544030f31ce0aa39fa165468b5c
suissitakwa <suissitakwa@gmail.com>
2026-06-30 12:04:02 -0700

    skip KafkaAdmin topic creation when kafka.enabled=false

--- src/main/java/com/retail_project/Kafka/config/KafkaConfig.java
+++ src/main/java/com/retail_project/Kafka/config/KafkaConfig.java
@@ -1,11 +1,13 @@
 package com.retail_project.Kafka.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@ConditionalOnProperty(name = "kafka.enabled", havingValue = "true", matchIfMissing = true)
 public class KafkaConfig {
     @Bean
     public NewTopic orderTopic(){
